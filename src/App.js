4ddd0f61ccbbe199dedd812b561ce221a375a4d7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-06 20:38:25 +0300

    props called

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Greet from "./components/Greet";
 import "./App.css";
 import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
-
+import Title from "./components/Title";
 function App() {
   return (
     <div className="App">
       <Greet />
       <Welcome />
       <Hello />
+      <Title name="Frontend Developer" />
+      <Title name="Mobile Developer" />
+      <Title name="Solodity Developer" />
     </div>
   );
 }
