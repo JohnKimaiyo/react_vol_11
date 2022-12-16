b60da64e1dac9b5e86b2a43187070a139fc53108
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-15 23:09:36 -0800

    context api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-
 import "./App.css";
 import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
+import ThemeContextProvider from "./context/ThemeContext";
 
 function App() {
   return (
     <div className="App">
-      <BookList/>
-      <Navbar/>
+      <ThemeContextProvider>
+        <BookList />
+        <Navbar />
+      </ThemeContextProvider>
     </div>
   );
 }
